4e8e60690f5acfab0032802cee5962542f5ec49c
Maks <rubelmaksum2404@gmail.com>
2025-04-18 20:43:45 +0300

    Add project files.

---
+++ ClientCar/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Configuration;
+
+namespace ClientCar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const string serverAddress = "127.0.0.1";
+        const int port = 4040;
+        IPEndPoint serverPoint;
+        TcpClient client;
+        NetworkStream ns = null;
+        StreamWriter sw = null;
+        StreamReader sr = null;
+        //ObservableCollection<MessageInfo> messages;
+        public MainWindow()
+        {
+            InitializeComponent();
+            client = new TcpClient();
+            
+            serverPoint = new IPEndPoint(IPAddress.Parse(serverAddress), port);
+           
+        }
+
+        private void SearchBtn(object sender, RoutedEventArgs e)
+        {
+            
+            string message = srcTextBox.Text;
+            srcTextBox.Text = "";
+            sw.WriteLine(message);
+            sw.Flush();
+            
+
+        }
+        
+        private async void Listener()
+        {
+            
+            while (true)
+            {
+                try
+                {
+                    
+                    string? message = await sr.ReadLineAsync();
+                    lb1.Items.Clear();
+                    lb1.Items.Add(message);
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                    break;
+                }
+
+            }
+        }
+
+        private void ConnectBtn(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                client.Connect(serverPoint);
+                ns = client.GetStream();
+
+                sr = new StreamReader(ns);
+                sw = new StreamWriter(ns);
+
+                Listener();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime;
+
+public class VinInfo
+{
+    public string country {  get; set; }
+    public string region { get; set; }
+    public string year { get; set; }
+
+    // для тесту 5YJSA1DP4CFS00842 або JH4KA7561PC008269
+
+}
+
+public class CharServer
+{
+
+    const int port = 4040;
+
+    TcpListener server;
+
+    public CharServer()
+    {
+        server = new TcpListener(new IPEndPoint(IPAddress.Parse("127.0.0.1"), port));
+    }
+    public void Start()
+    {
+       
+
+        server.Start();
+        Console.WriteLine("Waiting for connection");
+        TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine("Connected!!!");
+        NetworkStream ns = client.GetStream();
+        StreamWriter sw = new StreamWriter(ns);
+        StreamReader sr = new StreamReader(ns);
+        while (true)
+        {
+            string vin = sr.ReadLine()!;
+            string url = $"https://api.api-ninjas.com/v1/vinlookup?vin={vin}";
+            VinInfo? vinInfo = null;
+            using (WebClient wclient = new WebClient())
+            {
+                wclient.Headers.Add("X-Api-Key", "16XbQpeMofTroqIM2xvmIw==8IzpyLQakNDzKW7F");
+                try
+                {
+                    string json = wclient.DownloadString(url);
+
+                    vinInfo = JsonConvert.DeserializeObject<VinInfo>(json);
+
+
+                }
+                catch (Exception ex)
+                { Console.WriteLine(ex.Message); }
+
+                Console.WriteLine($"{DateTime.Now.ToLongTimeString()}  -- {vin} --  from {client.Client.LocalEndPoint}");
+
+                if (vinInfo != null)
+                {
+                    sw.WriteLine($"Year : {vinInfo.year} | Country : {vinInfo.country} | Region : {vinInfo.region}");
+                    sw.Flush();
+                }
+                else
+                {
+                    sw.WriteLine("Not found");
+                }
+
+
+            }
+
+
+
+
+        }
+
+    }
+}
+
+internal class Program
+{
+    
+    private static void Main(string[] args)
+    {
+        CharServer server_ = new CharServer();
+        server_.Start();
+    }
+}
